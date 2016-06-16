ab4e398838115e593711cb58ffdd6b7ec5d1242f
Chuyen Nguyen <chuyen.nguyen@synova.com.vn>
2016-06-16 18:17:20 +0700

    update build

--- web/scripts/login/loginCtrl.js
+++ web/scripts/login/loginCtrl.js
@@ -27,6 +27,9 @@
               if ($scope.roles.join().indexOf('ROLE_ADMIN') >= 0 ) {
                 $location.path('/clients');
                 $rootScope.isAdmin = true;
+                $rootScope.permissionUserGroup = {'VIEW': true, 'CREATE': true, 'EDIT': true, 'DELETE': true};
+                $rootScope.permissionUser = {'VIEW': true, 'CREATE': true, 'EDIT': true, 'DELETE': true};
+                $rootScope.permissionHandbook = {'VIEW': true, 'CREATE': true, 'EDIT': true, 'DELETE': true};
                 return $route.reload();
               }else {
                 return aREST.get($scope.username, $scope.password, config.path.baseURL + system.data._links.logged_in_position.href).then(function (position) {
@@ -38,6 +41,67 @@
                     localStorageService.cookie.set('permissionUserGroup', employer.data._links.user_groups.actions, 1);
                     localStorageService.cookie.set('permissionUser', employer.data._links.positions.actions, 1);
                     localStorageService.cookie.set('permissionHandbook', employer.data._links.handbooks.actions, 1);
+
+
+                    var permissionUserGroup = employer.data._links.user_groups.actions;
+                    if (permissionUserGroup != undefined) {
+                      $rootScope.permissionUserGroup = {'VIEW': false, 'CREATE': false, 'EDIT': false, 'DELETE': false};
+                      if (permissionUserGroup.join().indexOf('OPERATE') > -1) {
+                        $rootScope.permissionUserGroup = {'VIEW': true, 'CREATE': true, 'EDIT': true, 'DELETE': true};
+                      } else {
+                        if (permissionUserGroup.join().indexOf('VIEW') > -1) {
+                          $rootScope.permissionUserGroup.VIEW = true;
+                        }
+                        if (permissionUserGroup.join().indexOf('CREATE') > -1) {
+                          $rootScope.permissionUserGroup.CREATE = true;
+                        }
+                        if (permissionUserGroup.join().indexOf('EDIT') > -1) {
+                          $rootScope.permissionUserGroup.EDIT = true;
+                        }
+                        if (permissionUserGroup.join().indexOf('DELETE') > -1) {
+                          $rootScope.permissionUserGroup.DELETE = true;
+                        }
+                      }
+                    }
+
+
+                    var permissionUser = employer.data._links.positions.actions;
+                    if (permissionUser != undefined) {
+                      $rootScope.permissionUser = {'VIEW': false, 'CREATE': false, 'EDIT': false, 'DELETE': false};
+                      if (permissionUser.join().indexOf('OPERATE') > -1) {
+                        $rootScope.permissionUser = {'VIEW': true, 'CREATE': true, 'EDIT': true, 'DELETE': true};
+                      } else {
+                        if (permissionUser.join().indexOf('CREATE') > -1) {
+                          $rootScope.permissionUser.CREATE = true;
+                        }
+                        if (permissionUser.join().indexOf('EDIT') > -1) {
+                          $rootScope.permissionUser.EDIT = true;
+                        }
+                        if (permissionUser.join().indexOf('DELETE') > -1) {
+                          $rootScope.permissionUser.DELETE = true;
+                        }
+                      }
+                    }
+                    var permissionHandbook = employer.data._links.handbooks.actions;
+                    if (permissionHandbook != undefined) {
+                      $rootScope.permissionHandbook = {'VIEW': false, 'CREATE': false, 'EDIT': false, 'DELETE': false};
+                      if (permissionHandbook.join().indexOf('OPERATE') > -1) {
+                        $rootScope.permissionHandbook = {'VIEW': true, 'CREATE': true, 'EDIT': true, 'DELETE': true};
+                      } else {
+                        if (permissionHandbook.join().indexOf('CREATE') > -1) {
+                          $rootScope.permissionHandbook.CREATE = true;
+                        }
+                        if (permissionHandbook.join().indexOf('EDIT') > -1) {
+                          $rootScope.permissionHandbook.EDIT = true;
+                        }
+                        if (permissionHandbook.join().indexOf('DELETE') > -1) {
+                          $rootScope.permissionHandbook.DELETE = true;
+                        }
+                        if (permissionHandbook.join().indexOf('VISIBILITY') > -1) {
+                          $rootScope.permissionHandbook.VISIBILITY = true;
+                        }
+                      }
+                    }
 
                     $location.path('/clients/' + employer.data.id + '/info');
                     $rootScope.isAdmin = false;
@@ -57,6 +121,11 @@
   ]).controller('logoutCtrl', [
     '$scope', '$location', 'localStorageService', '$http', '$route', '$rootScope', function($scope, $location, localStorageService, $http, $route, $rootScope) {
       localStorageService.cookie.remove('user');
+      localStorageService.cookie.remove('permissionHandbook');
+      localStorageService.cookie.remove('permissionUserGroup');
+      localStorageService.cookie.remove('permissionUser');
+      localStorageService.cookie.remove('employerId');
+      localStorageService.cookie.remove('positionId');
       delete $http.defaults.headers.common['x-session'];
       $rootScope.isLoggedIn = false;
       $location.path('/login');
